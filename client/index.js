3c7bb49ecfc3dc294cf32d130d786f88bc839acc
Cindy Nguyen <cnguyen0@uw.edu>
2018-02-15 13:36:34 -0800

    added port to index

--- client/index.js
+++ client/index.js
@@ -1,6 +1,14 @@
+'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import { BrowserRouter } from 'react-router-dom'
+
+const PORT = process.env.PORT || 8080;
+
+App.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
 
 ReactDOM.render(  <BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
